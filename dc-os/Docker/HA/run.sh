32d8fe969945e08c4f33db6aef5d9764041c2228
jainish shah <jainishshah@yahoo.com>
2017-02-24 17:01:44 -0800

    no message

--- dc-os/Docker/HA/run.sh
+++ dc-os/Docker/HA/run.sh
@@ -43,7 +43,7 @@ function setInitialConfiguration {
         mkdir -p /var/opt/jfrog/artifactory/etc/plugins
         cp -f /tmp/internalUser.groovy /var/opt/jfrog/artifactory/etc/plugins/internalUser.groovy
     fi
-
+     cp -f /tmp/binarystore.xml /var/opt/jfrog/artifactory/etc/binarystore.xml
      # Artifactory's external server name
     : ${ART_SERVER_NAME:=artifactory-cluster}
     # Artifactory's port method, default to PORTPERREPO (can be SUBDOMAIN)
@@ -56,12 +56,12 @@ function setInitialConfiguration {
         mv /tmp/artifactory.config.xml /var/opt/jfrog/artifactory/etc/artifactory.config.import.xml
     # On later runs, we'll re import the latest configuration to change the instance port
     else
-        if [ -f /var/opt/jfrog/artifactory/etc/artifactory.config.latest.xml ]; then
+#        if [ -f /var/opt/jfrog/artifactory/etc/artifactory.config.latest.xml ]; then
             cp /var/opt/jfrog/artifactory/etc/artifactory.config.latest.xml /var/opt/jfrog/artifactory/etc/artifactory.config.import.xml
-        else
-        # If not we take the bootstrap one, and import it
-            cp /var/opt/jfrog/artifactory/etc/artifactory.config.bootstrap.xml /var/opt/jfrog/artifactory/etc/artifactory.config.import.xml
-        fi
+#        else
+#        # If not we take the bootstrap one, and import it
+#            cp /var/opt/jfrog/artifactory/etc/artifactory.config.bootstrap.xml /var/opt/jfrog/artifactory/etc/artifactory.config.import.xml
+#        fi
         # Changing the instance port
         sed -i -e "s,<artifactoryPort>\(.*\)</artifactoryPort>,<artifactoryPort>$INSTANCE_PORT</artifactoryPort>,g" /var/opt/jfrog/artifactory/etc/artifactory.config.import.xml
     fi
