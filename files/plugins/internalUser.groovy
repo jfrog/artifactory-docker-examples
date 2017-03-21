f0b2434e0b14a58a38a6900785dfbed0decd254f
jainish shah <jainishshah@yahoo.com>
2017-03-21 15:14:18 -0700

    Plugin to clean inactive servers from artifactory server table

--- files/plugins/internalUser.groovy
+++ files/plugins/internalUser.groovy
@@ -6,10 +6,19 @@ import org.artifactory.factory.InfoFactoryHolder
 import org.artifactory.security.UserInfo
 import org.artifactory.security.props.auth.ApiKeyManager
 import org.artifactory.security.props.auth.model.TokenKeyValue
+import org.artifactory.storage.db.servers.service.ArtifactoryServersCommonService
+import org.slf4j.Logger
 
 @Field
 final String internalUserName = '_internal'
 createInternalUserIfNeeded()
+
+jobs {
+    clean(cron: "1/30 * * * * ?") {
+        def artifactoryServersCommonService = ContextHelper.get().beanForType(ArtifactoryServersCommonService)
+        new ArtifactoryInactiveServersCleaner(artifactoryServersCommonService, log).cleanInactiveArtifactoryServers()
+    }
+}
 
 void createInternalUserIfNeeded() {
     String password='b6a50c8a15ece8753e37cbe5700bf84f'
@@ -34,4 +43,28 @@ void createInternalUserIfNeeded() {
     }
 }
 
+public class ArtifactoryInactiveServersCleaner {
 
+    private ArtifactoryServersCommonService artifactoryServersCommonService
+    private Logger log
+
+    ArtifactoryInactiveServersCleaner(ArtifactoryServersCommonService artifactoryServersCommonService, Logger log) {
+        this.artifactoryServersCommonService = artifactoryServersCommonService
+        this.log = log
+    }
+
+    List<String> cleanInactiveArtifactoryServers() {
+        String primaryId = artifactoryServersCommonService.getRunningHaPrimary()?.serverId
+        if (primaryId) {
+            List<String> allMembers = artifactoryServersCommonService.getAllArtifactoryServers().collect({ it.serverId })
+            List<String> activeMembersIds = artifactoryServersCommonService.getOtherActiveMembers().collect({ it.serverId })
+            List<String> inactiveMembers = allMembers - activeMembersIds - primaryId
+            log.info "Running inactive artifactory servers cleaning task, found ${inactiveMembers.size()} inactive servers to remove"
+            for (String inactiveMember : inactiveMembers) {
+                artifactoryServersCommonService.removeServer(inactiveMember)
+            }
+            return inactiveMembers
+        }
+    }
+
+}
