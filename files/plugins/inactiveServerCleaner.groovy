c291e375310db5254a05d55658b13fde06d6e606
jainish shah <jainishshah@yahoo.com>
2018-02-02 12:24:04 -0800

    added logging and exception handing in plugin and bumped chart version

--- files/plugins/inactiveServerCleaner.groovy
+++ files/plugins/inactiveServerCleaner.groovy
@@ -2,10 +2,11 @@ import org.artifactory.state.ArtifactoryServerState
 import org.artifactory.storage.db.servers.service.ArtifactoryServersCommonService
 import org.artifactory.common.ConstantValues
 import org.slf4j.Logger
+
 import java.util.concurrent.TimeUnit
 
 jobs {
-    clean(cron: "* 0/1 * * * ?") {
+    clean(cron: "0 * * * * ?") {
         def artifactoryServersCommonService = ctx.beanForType(ArtifactoryServersCommonService)
         def artifactoryInactiveServerCleaner = new ArtifactoryInactiveServersCleaner(artifactoryServersCommonService, log)
         artifactoryInactiveServerCleaner.cleanInactiveArtifactoryServers()
@@ -23,15 +24,22 @@ public class ArtifactoryInactiveServersCleaner {
     }
 
     def cleanInactiveArtifactoryServers() {
+        log.info "Executing inactive artifactory servers cleaner plugin"
         List<String> allMembers = artifactoryServersCommonService.getAllArtifactoryServers()
         for (member in allMembers) {
             def heartbeat = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - member.getLastHeartbeat())
-            def noheartbeat = heartbeat > ConstantValues.haHeartbeatStaleIntervalSecs.getInt()
-            if (member.getServerState() == ArtifactoryServerState.UNAVAILABLE || noheartbeat) {
-                log.info "Running inactive artifactory servers cleaning task, found ${member.serverId} inactive servers to " +
-                        "remove"
-                artifactoryServersCommonService.removeServer(member.serverId)
+            def noHeartbeat = heartbeat > ConstantValues.haHeartbeatStaleIntervalSecs.getInt()
+            if (member.getServerState() == ArtifactoryServerState.UNAVAILABLE || noHeartbeat) {
+                try {
+                    log.info "Running inactive artifactory servers cleaning task, found ${member.serverId} inactive servers to " +
+                            "remove"
+                    artifactoryServersCommonService.removeServer(member.serverId)
+
+                }catch (Exception e){
+                    log.error "Error: Not able to remove ${member.serverId}, ${e.message}"
+                }
             }
         }
+        log.info "No inactive servers found"
     }
 }
