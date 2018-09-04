6e8ae2dc5cb51353e5eea9a67716f44a7452977a
CrazyMax <crazy-max@users.noreply.github.com>
2018-09-04 15:58:05 +0200

    Fix prepareHostEnv script

--- docker-compose/xray/prepareHostEnv.sh
+++ docker-compose/xray/prepareHostEnv.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This file is for preparing all the needed files and directories on the host.
 
-source ./.env
-
 SCRIPT_DIR=$(dirname $0)
 OS_NAME=$(uname)
 
@@ -15,8 +13,20 @@ if [ "${OS_NAME}" = "Linux" ] && [ "$EUID" != 0 ]; then
     errorExit "This script must be run as root or with sudo"
 fi
 
-echo "Creating ${XRAY_MOUNT_ROOT}"
-mkdir -p ${XRAY_MOUNT_ROOT}/xray
+if [ ! -f ./.env ]; then
+    errorExit ".env file does not exist in $SCRIPT_DIR"
+fi
 
-echo "Setting needed ownerships on ${XRAY_MOUNT_ROOT}"
-chown -R ${XRAY_USER_ID}:${XRAY_USER_ID} ${XRAY_MOUNT_ROOT}/xray || errorExit "Setting ownership of ${XRAY_MOUNT_ROOT}/xray to ${XRAY_USER_ID} failed"
+source ./.env
+
+if [ ! -d ${XRAY_MOUNT_ROOT}/xray ]; then
+    echo "Creating ${XRAY_MOUNT_ROOT}/xray"
+    mkdir -p ${XRAY_MOUNT_ROOT}/xray
+fi
+
+if [ $(stat -c '%u' ${XRAY_MOUNT_ROOT}/xray) != "${XRAY_USER_ID}" ] || [ $(stat -c '%g' ${XRAY_MOUNT_ROOT}/xray) != "${XRAY_USER_ID}" ]; then
+    echo "Setting needed ownerships on ${XRAY_MOUNT_ROOT}/xray"
+    chown -R ${XRAY_USER_ID}:${XRAY_USER_ID} ${XRAY_MOUNT_ROOT}/xray || errorExit "Setting ownership of ${XRAY_MOUNT_ROOT}/xray to ${XRAY_USER_ID} failed"
+fi
+
+echo "Done!"
